819a19a80590685d3da0812db8ab5b3aa286887c
namonak <namonak@gmail.com>
2023-09-19 12:17:58 +0900

    refactor(boj.1018): wrap readline logic to exclude during tests

--- src/main/boj.1018.ts
+++ src/main/boj.1018.ts
@@ -1,18 +1,20 @@
 import * as readline from 'readline';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+if (process.env.NODE_ENV !== 'test') {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-let input: number[] = [];
+  let input: number[] = [];
 
-rl.on('line', function (line: string) {
-  input = line.split(' ').map((el: string) => parseInt(el));
-}).on('close', function () {
-  console.log(solution(input[0], input[1]));
-  process.exit();
-});
+  rl.on('line', function (line: string) {
+    input = line.split(' ').map((el: string) => parseInt(el));
+  }).on('close', function () {
+    console.log(solution(input[0], input[1]));
+    process.exit();
+  });
+}
 
 export function solution(a: number, b: number): number {
   return a + b;
