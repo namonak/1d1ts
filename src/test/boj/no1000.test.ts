1cf4c3698f1ba62816315a858b6a283454ea9dbf
namonak <namonak@gmail.com>
2024-07-15 22:34:50 +0900

    refactor: update no1000.ts to improve solution function and input handling
     - Modified no1000.ts to streamline the solution function by changing its input to a single string argument and returning a string result.
     - Removed the readline interface from no1000.ts.

--- src/test/boj/no1000.test.ts
+++ src/test/boj/no1000.test.ts
@@ -1,5 +1,5 @@
 import { solution } from '../../main/boj/no1000';
 
 test('A+B : https://www.acmicpc.net/problem/1000', () => {
-  expect(solution(1, 2)).toBe(3);
+  expect(solution('1, 2')).toBe('3');
 });
