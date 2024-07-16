e13bdb643787c00aeb3887b4db15638be3b6eeef
namonak <namonak@gmail.com>
2024-07-16 16:16:23 +0900

    refactor: update main.ts to use No1001 class for solving problem
     - Replaced reference to No1000 with No1001 in main.ts.
     - Refactored no1001.ts to define the No1001 class with an async solve method, handling input via readline.
     - Updated no1001.test.ts to test the No1001 class using async/await and Readable streams.

--- src/main/boj/main.ts
+++ src/main/boj/main.ts
@@ -1,10 +1,10 @@
 import * as fs from 'fs';
-import { No1000 } from '../../main/boj/no1000';
+import { No1001 } from '../../main/boj/no1001';
 
 async function main() {
   try {
     const input = fs.createReadStream('/dev/stdin');
-    const result = await new No1000().solve(input);
+    const result = await new No1001().solve(input);
     console.log(result);
     process.exit();
   } catch (err) {
--- src/test/boj/no1001.test.ts
+++ src/test/boj/no1001.test.ts
@@ -1,5 +1,10 @@
-import { solution } from '../../main/boj/no1001';
+import { No1001 } from '../../main/boj/no1001';
+import { Readable } from 'stream';
 
-test('A-B : https://www.acmicpc.net/problem/1001', () => {
-  expect(solution('3, 2')).toBe('1');
+test('A+B : https://www.acmicpc.net/problem/1001', async () => {
+  const given = '3 2';
+  const expected = '1';
+
+  const result = await new No1001().solve(Readable.from(given));
+  expect(result).toBe(expected);
 });
